c96683866273161252fd8e84039e6b369f9c2f13
Paolo Botta <botta717@gmail.com>
2026-09-17 16:36:42 +0200

    update submit scripts

--- submit_acc_gpu.sh
+++ submit_acc_gpu.sh
@@ -6,14 +6,14 @@
 #SBATCH --partition=boost_usr_prod
 #SBATCH --time=0:30:00
 #SBATCH --mem=0
-#SBATCH --job-name=run_omp_cooling
+#SBATCH --job-name=run_acc_gpu_cooling
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 #SBATCH --gres=gpu:1
 #SBATCH --qos=boost_qos_dbg
 #SBATCH --exclusive
 
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export ACC_NUM_CORES=${SLURM_CPUS_PER_TASK}
 source scripts/env.leonardo.sh
-./install/bin/cooling_openmp ./input/Cooling.in none ./output/Cooling_omp.csv
+./install/bin/cooling_openacc_gpu ./input/Cooling.in none ./output/Cooling_acc_gpu.csv
 
--- submit_cpp.sh
+++ submit_cpp.sh
@@ -15,5 +15,5 @@
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 source scripts/env.leonardo.sh
-./install/bin/cooling_serial ./input/Cooling.in ./output/Cooling_cpp.h5 ./output/Cooling_cpp.csv
+./install/bin/cooling_serial ./input/Cooling.in none ./output/Cooling_cpp.csv
 
